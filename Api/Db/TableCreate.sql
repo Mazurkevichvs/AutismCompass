cf6e97990acfb08d986438c0a9a080337dd57448
Bogda <79489696+Bohdan1337max@users.noreply.github.com>
2023-07-22 22:14:16 +0200

    init
    init

---
+++ Api/Db/TableCreate.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE Gathering
+(
+    id          SERIAL PRIMARY KEY,
+    City        VARCHAR(30) NOT NULL,
+    Name        VARCHAR(30) NOT NULL,
+    Description VARCHAR(90) NULL
+)
---
+++ Api/ProjectAutism/Controllers/GatheringController.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ProjectAutism.Data;
+using ProjectAutism.Models;
+
+namespace ProjectAutism.Controllers;
+
+[ApiController]
+[Route("events")]
+public class GatheringController : ControllerBase
+{
+   // public static List<Gathering> Gatherings = new List<Gathering>();
+    private AutismDbContext _context;
+
+    public GatheringController(AutismDbContext context)
+    {
+        _context = context;
+    }
+    
+    [HttpGet]
+    public IActionResult GetEvents()
+    {
+        var gatherings = _context.Gatherings.ToList();
+        return Ok(gatherings);
+    }
+
+    [HttpPost]
+    public IActionResult CreateGathering(Gathering gathering)
+    {
+        
+        var addedGathering = _context.Gatherings.Add(gathering);
+        _context.SaveChanges();
+        return Ok(addedGathering.Entity);
+    }
+    [HttpGet("{id}")]
+    public IActionResult FindById(int id)
+    {
+        var gathering = _context.Gatherings.FirstOrDefault(g => g.Id == id );
+        if (gathering == null)
+        {
+            return NotFound();
+        }
+        return Ok(gathering);
+    }
+    
+    
+    [HttpGet("Serch")]
+    public IActionResult FindByName( [FromQuery] string name)
+    {
+
+        var gatherings = _context.Gatherings.Where(gathering => gathering.Name == name).ToList();
+        return Ok(gatherings);
+    }
+}
