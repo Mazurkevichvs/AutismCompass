dd2b87e2b3a8fe6708e95e03deb67cb907843dba
Bogdan <gdx148063@student.wsb.gda.pl>
2023-12-10 18:22:36 +0100

    Change gathering model and sql types

--- Api/ProjectAutism/Program.cs
+++ Api/ProjectAutism/Program.cs
@@ -23,9 +23,28 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
-app.UseHttpsRedirection();
+app.UseRouting();
 
 app.UseAuthorization();
+#pragma warning disable ASP0014
+// Ensures the correct order of middleware when used with SPA proxy
+//app.UseEndpoints(_ => { });
+#pragma warning restore ASP0014
+
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSpa(x =>
+    {
+        var spaConfigSection = app.Configuration.GetSection("SpaDevelopmentServer");
+        var spaServer = spaConfigSection["SpaServer"];
+        var spaUrl = spaConfigSection[$"Url:{spaServer}"];
+        x.UseProxyToSpaDevelopmentServer(spaUrl!);
+    });
+}
+
+app.UseHttpsRedirection();
 
 app.MapControllers();
+
 app.Run();
