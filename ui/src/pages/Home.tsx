b19ef0ac6b39639d11faa2e851fff28457222245
Mazurkevichvs <mazurkevichvs@gmail.com>
2023-12-11 21:56:16 +0100

    change slider for head and messages

--- ui/src/pages/Home.tsx
+++ ui/src/pages/Home.tsx
@@ -1,13 +1,28 @@
 import React from 'react';
 import { AboutSection, FactsAndMyths, Slider } from '../components';
 
+const HEADSLIDERTITLES = [
+  { title: 'Lorem ipsum dolor sit amet consectetur.' },
+  { title: 'Lorem ipsum dolor sit amet consectetur.' },
+  { title: 'Lorem ipsum dolor sit amet consectetur.' },
+  { title: 'Lorem ipsum dolor sit amet consectetur.' },
+];
+
+const MESSAGESLIDERTITLES = [
+  { title: 'Stimy – Jakie jest znaczenie dla osób na spektrum autyzmu?Stimy to powtarzanie ruchów, dźwięków lub manipulowanie przedmiotami w sposób regularny, takie jak machanie rękoma czy wiercenie się. Dlaczego osoby z autyzmem angażują się w stimy?' },
+  { title: 'Stimy – Jakie jest znaczenie dla osób na spektrum autyzmu?Stimy to powtarzanie ruchów, dźwięków lub manipulowanie przedmiotami w sposób regularny, takie jak machanie rękoma czy wiercenie się. Dlaczego osoby z autyzmem angażują się w stimy?' },
+  { title: 'Stimy – Jakie jest znaczenie dla osób na spektrum autyzmu?Stimy to powtarzanie ruchów, dźwięków lub manipulowanie przedmiotami w sposób regularny, takie jak machanie rękoma czy wiercenie się. Dlaczego osoby z autyzmem angażują się w stimy?' },
+  { title: 'Stimy – Jakie jest znaczenie dla osób na spektrum autyzmu?Stimy to powtarzanie ruchów, dźwięków lub manipulowanie przedmiotami w sposób regularny, takie jak machanie rękoma czy wiercenie się. Dlaczego osoby z autyzmem angażują się w stimy?' },
+  
+];
+
 const Home: React.FC = () => {
   return (
     <>
-        <Slider />
-        <AboutSection />
-        <Slider />
-        <FactsAndMyths/>
+      <Slider isHead={true} data={HEADSLIDERTITLES}/>
+      <AboutSection />
+      <Slider isHead={false} data={MESSAGESLIDERTITLES}/>
+      <FactsAndMyths />
     </>
   );
 };
